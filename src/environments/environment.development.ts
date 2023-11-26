722491c64905844d47f0e0f9e689850aed69d0ee
EnriqueVargasSoto <45204929+EnriqueVargasSoto@users.noreply.github.com>
2023-11-25 23:47:08 -0500

    25/11/2023 23.47

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://backend.pos.atiendo.pe/api/',//'http://127.0.0.1:8000/api/',//'http://www.valorx.net:8080/Magicxpi4.12/MgWebRequester.dll?appname=IFSValorX&prgname=HTTP&arguments=-AHTTPVLXRest%23ListItems',
+  apiUrl: 'https://backend-pos.atiendo.pe/api/',//'http://127.0.0.1:8000/api/',//'http://www.valorx.net:8080/Magicxpi4.12/MgWebRequester.dll?appname=IFSValorX&prgname=HTTP&arguments=-AHTTPVLXRest%23ListItems',
   Compania : '0066',
   Sucursal : '01',
   lista_precio: '1'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend.pos.atiendo.pe/api/',//'http://www.valorx.net/Magicxpi4.12/MgWebRequester.dll?appname=IFSValorX&prgname=HTTP&arguments=-AHTTPVLXRest%23ListItems',
+  apiUrl: 'https://backend-pos.atiendo.pe/api/',//'http://www.valorx.net/Magicxpi4.12/MgWebRequester.dll?appname=IFSValorX&prgname=HTTP&arguments=-AHTTPVLXRest%23ListItems',
   Compania : '0066',
   Sucursal : '01',
   lista_precio: '1'
