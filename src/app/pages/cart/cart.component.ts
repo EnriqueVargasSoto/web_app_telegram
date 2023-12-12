0e3279da44d20b4eafa1715ce50a27bf714c3a5d
EnriqueVargasSoto <45204929+EnriqueVargasSoto@users.noreply.github.com>
2023-12-12 11:28:08 -0500

    12/12/2023 11.28

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { ApiService } from 'src/app/utils/api/api.service';
 import { CartService } from 'src/app/utils/cart/cart.service';
+import { environment } from 'src/environments/environment';
+import { Modal } from 'flowbite';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -12,13 +15,23 @@ export class CartComponent {
   carrito: any[] = [];
   total: number = 0.00;
 
-  constructor(private apiService: ApiService, private cart: CartService){
+  private localStorageKey = 'user';
+  usuario: any;
+
+  modal: any;
+
+  constructor(private apiService: ApiService, private cart: CartService, private router: Router){
 
   }
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
+
+    const targetEl = document.getElementById('popup-modal');
+    this.modal = new Modal(targetEl);
+    const userJson = localStorage.getItem(this.localStorageKey);
+    this.usuario = JSON.parse(userJson!);
     this.setCart();
 
   }
@@ -41,9 +54,75 @@ export class CartComponent {
     }
   }
 
-  guardarPedido(){
-    if (this.carrito.length > 0) {
+  async guardarPedido(){
+
+    let items: any[] = [];
+
+    for (let i = 0; i < this.carrito.length; i++) {
+      const element = {
+        "sku" : this.carrito[i]['item_cod'],
+        "procedimiento" : "003",
+        "unidad_medida" : this.carrito[i]['unidad_man'],
+        "cantidad_pedida" : this.carrito[i]['cantidad'],
+        "lista_precio" : this.usuario['listprecio'],
+      }
+
+      items.push(element);
 
     }
+
+    let data: any = {
+      "nro_pedido_origen" : 1234567890,
+      "fecha_pedido" : "2023-12-12",
+      "vendedor" : "10098407338",
+      "centro_distribucion" : "2",
+      "zona" : "2000",
+      "ruta" : "2097",
+      "forma_pago" : "001",
+      "moneda" : "PEN",
+      "fecha_entrega" : null,
+      "hora_entrega" : null,
+      "detalle_pedido" : [
+        {
+          "cliente" : [
+            {
+              "nro_documento_ide" : this.usuario['user_ndocid'],
+              "tipo_documento_ide" : this.usuario['user_tdocid'],
+              "nombre_completo" : this.usuario['user_name'],
+              "direccion_fiscal" : "",
+              "ubigeo_dirfiscal" : this.usuario['user_ubigeo'],
+              "direccion_despacho" : "",
+              "ubigeo_dirdespacho" : this.usuario['user_ubigeo'],
+              "telefono" : this.usuario['user_phone'],
+              "modulo" : "9720",
+              "latitud" : "-16.47293217480",
+              "longitud" : "-71.52866047464",
+              "items" : items
+            }
+          ]
+        }
+      ]
+    }
+    if (this.carrito.length > 0) {
+      let body = {
+        'compania' : environment.Compania,
+        'sucursal' : environment.Sucursal,
+        'order' : data
+      };
+      (await this.apiService.consulta('save-order', 'post', body)).subscribe((resp) => {
+        console.log(resp);
+        if (resp['status_code'] == 200) {
+          this.modal.show();
+        }
+      });
+    }else{
+
+    }
+  }
+
+  cerrarModal(){
+    this.cart.cart = [];
+    this.modal.hide();
+    this.router.navigate(['/']);
   }
 }
