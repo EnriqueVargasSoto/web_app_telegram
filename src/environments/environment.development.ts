390fa899530aa851217a35f09eab84f14c0759ea
EnriqueVargasSoto <45204929+EnriqueVargasSoto@users.noreply.github.com>
2023-12-20 20:08:30 -0500

    20/12/2023 20.08

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'https://backend-pos.atiendo.pe/api/',//'http://127.0.0.1:8000/api/',//'http://www.valorx.net:8080/Magicxpi4.12/MgWebRequester.dll?appname=IFSValorX&prgname=HTTP&arguments=-AHTTPVLXRest%23ListItems',
-  Compania : '0054',
-  Sucursal : '02',
+  Compania : '0004',
+  Sucursal : '01',
   lista_precio: '1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://backend-pos.atiendo.pe/api/',//'http://www.valorx.net/Magicxpi4.12/MgWebRequester.dll?appname=IFSValorX&prgname=HTTP&arguments=-AHTTPVLXRest%23ListItems',
-  Compania : '0054',
-  Sucursal : '02',
+  Compania : '0004',
+  Sucursal : '01',
   lista_precio: '1'
 };
